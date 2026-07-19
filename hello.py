78429ab2eca8e0188c810a32d47db06a24e0f364
jillur984 <jillur.cse.bd@gmail.com>
2026-07-19 16:26:55 +0600

    learn python for IOT

---
+++ hello.py
@@ -0,0 +1,13 @@
+a=float(input("Enter The First Number: "))
+b=float(input("Enter The Second Number: "))
+
+
+sum=a+b
+sub=a-b
+mul=a*b 
+div=a/b
+
+print("The sum of Two Number is: ",sum)
+print("The subtraction of Two Number is: ",sub)
+print("The multiplication of Two Number is: ",mul)
+print("The division of Two Number is: ",div)
